47ad8bfa7c655ffee05836a73b252350c8ca1eef
Rizky Dana <rizkydana14@gmail.com>
2025-12-29 11:28:55 +0800

    feat(astro): add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
+  adapter: cloudflare(),
 });
