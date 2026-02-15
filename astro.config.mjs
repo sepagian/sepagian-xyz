8c1440c29ff7071a2de96cb605d9941be0d33ccb
Rizky Dana <rizkydana14@gmail.com>
2026-02-15 12:39:34 +0800

    style: standardize formatting and update project configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
-import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
-import cloudflare from "@astrojs/cloudflare";
-
 import playformFormat from "@playform/format";
 import Biome from "@playform/format";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-	site: "https://astro-nano-demo.vercel.app",
-	integrations: [mdx(), sitemap(), tailwind(), playformFormat(), Biome()],
-	adapter: cloudflare(),
+  site: "https://astro-nano-demo.vercel.app",
+  integrations: [mdx(), sitemap(), tailwind(), playformFormat(), Biome()],
+  adapter: cloudflare(),
 });
