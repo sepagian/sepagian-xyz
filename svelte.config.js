e94f66a962a2ef25e4ee75c5146c48a612fe5204
Rizky Dana <rizkydana14@gmail.com>
2026-05-28 19:20:22 +0800

    chore(svelte): add vitePreprocess and improve config formatting

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from "@sveltejs/adapter-cloudflare";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import UnoCSS from "@unocss/svelte-scoped/preprocess";
 
 const regex = /[/\\]/;
@@ -10,7 +11,10 @@ const config = {
     runes: ({ filename }) => (filename.split(regex).includes("node_modules") ? undefined : true),
   },
   kit: { adapter: adapter() },
-  preprocess: [UnoCSS({ configOrPath: "./uno.config.ts", exclude: ["./node_modules"] })],
+  preprocess: [
+    vitePreprocess(),
+    UnoCSS({ configOrPath: "./uno.config.ts", exclude: ["./node_modules"] }),
+  ],
   extensions: [".svelte", ".svx", ".md"],
 };
 
