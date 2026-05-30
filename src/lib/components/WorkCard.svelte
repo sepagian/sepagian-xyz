1328a412d167e0615dfd36e8c71f125feaf37660
Rizky Dana <rizkydana14@gmail.com>
2026-05-30 09:37:51 +0800

    style(WorkCard): refactor card layout to grid-friendly aspect ratio design

--- src/lib/components/WorkCard.svelte
+++ src/lib/components/WorkCard.svelte
@@ -10,18 +10,24 @@
 <li>
   <a
     {href}
-    class="block group border border-border rounded-md p-4 hover:(bg-muted) transition-all"
+    class="border border-border rounded-md p-2 hover:(bg-muted) transition-all aspect-square flex flex-col gap-2 h-full"
   >
     {#if work.image}
       <img
         src={urlFor(work.image).width(600).height(400).url()}
         alt={work.image.alt ?? work.title}
-        class="w-full h-48 object-cover rounded-md"
+        class="w-full rounded-sm border border-border aspect-3/2"
         loading="lazy"
       >
+    {:else}
+      <div
+        class="bg-muted aspect-3/2 w-full rounded-sm border border-border"
+      ></div>
     {/if}
-    <h2 class="text-base font-semibold">{work.title}</h2>
-    <p class="text-sm">{work.description}</p>
-    <TagBadge tags={work.tags} />
+    <div class="h-fit">
+      <h2 class="text-base font-semibold">{work.title}</h2>
+      <p class="text-sm">{work.description}</p>
+      <TagBadge tags={work.tags} />
+    </div>
   </a>
 </li>
